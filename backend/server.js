f05a3f7f15146c03fbdc61cb852aa78a8b691c15
Harsh Budhauliya <taliyotechnologies@gmail.com>
2025-07-23 20:25:34 +0530

    fix: update CORS to allow only flixconvert.taliyotechnologies.com and localhost

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['https://flix-convert.vercel.app', 'http://localhost:5173'],
+  origin: ['https://flixconvert.taliyotechnologies.com', 'http://localhost:5173'],
   credentials: true
 }));
 app.use(helmet());
