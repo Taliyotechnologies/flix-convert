2f4175c71feba8aa515e4ae9911f979e034a850c
Harsh Budhauliya <taliyotechnologies@gmail.com>
2025-07-26 03:11:26 +0530

    fix: remove white gaps around website edges with CSS reset and proper viewport styling

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
 import './components/Navbar.css';
 import './components/Footer.css';
 import './pages/Home.css';
 
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-} 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+); 
